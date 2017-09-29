d6100668f6d25c92fa69fc5aa3fce343655c800d
harrisonhadaway <harrisonleehadaway@gmail.com>
2017-09-29 12:31:45 -0400

    long way to go and a short time to get there

---
+++ app/main.js
@@ -0,0 +1,20 @@
+var seasonal1 = document.write("seasonal1");
+
+
+
+function (){
+
+for (var i = products.length - 1; i >= 0; i--) {
+	products[i]
+}
+
+};
+
+
+var products = {
+    : "John",
+    lastName : "Doe",
+    id       :  5566
+};
+document.getElementById("demo").innerHTML =
+person.firstName + " " + person.lastName;
